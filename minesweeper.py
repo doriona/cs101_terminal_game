1169c2151bc392b51569e7f481370182bd4e2600
doriona <82111572+doriona@users.noreply.github.com>
2021-10-13 15:25:54 +0200

    add function to print board

--- minesweeper.py
+++ minesweeper.py
@@ -13,45 +13,76 @@ def get_mine_positions(board, board_width, board_height, num_mines):
         y = random.randint(0, board_height -1)
         pos = str(x) + ',' + str(y)
         if board.get(pos) == None:
-            board[pos] = 'MINE'
+            board[pos] = 'M'
             mine_count += 1
-            #print("Board x: {x} and y: {y} = {mine}".format(x = x, y = y , mine = board[key]))
+            #print("Board x: {x} and y: {y} = {mine}".
+            #        format(x = x, y = y , mine = board[key]))
     return board
 
 def get_mine_counts(board, board_width, board_height):
     board2 = board.copy()
     for pos, value in board.items():
-        print("pos: {pos} and value: {value}".format(pos = pos, value = value))
-        if value == "MINE":
+        #print("pos: {pos} and value: {value}".
+        #        format(pos = pos, value = value))
+        if value == "M":
             x, y = pos.split(',')
-            print("x: {x}, y: {y}".format(x = x, y = y))
+            #print("x: {x}, y: {y}".format(x = x, y = y))
             for i in range(int(x) - 1, int(x) + 2):
                 if i >= 0:
                     for j in range(int(y) - 1, int(y) + 2):
                         if j >= 0:
                             current = str(i) + ',' + str(j)
-                            print("current: {0}".format(current))
                             if board.get(current) != None:
-                                if board[current] == "MINE":
-                                    board2[current] = "MINE"
+                                if board[current] == "M":
+                                    board2[current] = "M"
                             if board2.get(current) == None:
                                 board2[current] = 1
-                            elif board2[current] == "MINE":
+                            elif board2[current] == "M":
                                 pass
                             else:
                                 board2[current] += 1
     return board2
-                     
+
+def init_visible(visible, board_width, board_height):
+    i = 0
+    while (i < board_width):
+        j = 0
+        while (j < board_height):
+            pos = str(i) + ',' + str(j)
+            visible[pos] = 1
+            j += 1
+        i += 1
+    return visible
+
+def print_board(board, visible, board_width, board_height):
+    i = 0
+    while (i < board_width):
+        j = 0
+        while (j < board_height):
+            pos = str(i) + ',' + str(j)
+            if visible[pos] == 1:
+                if board.get(pos) != None:
+                    print(board[pos], end = " ")
+                else:
+                    print("0", end = " ")
+            j += 1
+        print("\n", end = "")
+        i += 1
 
 board = {}
+visible = {}
+visible = init_visible(visible, 5, 5)
 board = get_mine_positions(board, 5, 5, 5)
 board = get_mine_counts(board, 5, 5)
 print("------------------------------------")
-for x in range(0, board_width):
-    for y in range(0,board_height):
-        pos = str(x) + ',' + str(y)
-        if (board.get(pos) != None):
-            if (board[pos] == "MINE"):
-                print("Mine in: x:{x} y:{y}".format(x = x, y = y))
-            else:
-                print("Number of mines close to {pos}: {num}".format(pos = pos, num = board[pos])) 
+print_board(board, visible, 5, 5)
+print("------------------------------------")
+#for x in range(0, board_width):
+#    for y in range(0,board_height):
+#        pos = str(x) + ',' + str(y)
+#        if (board.get(pos) != None):
+#            if (board[pos] == "M"):
+#                print("Mine in: x:{x} y:{y}".format(x = x, y = y))
+#            else:
+#                print("Number of mines close to {pos}: {num}".
+#                        format(pos = pos, num = board[pos]))
